600e04cd777bfb97daf48d97455fc3b324a5ac80
LEVYABDI <kevyabdi@gmail.com>
2025-08-13 01:52:36 +0300

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -54,3 +54,56 @@ class Database:
 
 
 db = Database(DB_URL, DB_NAME)
+# Don't Remove Credit @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
+import motor.motor_asyncio
+from config import DB_URL, DB_NAME
+
+class Database:
+    def __init__(self, uri, database_name):
+        self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
+        self.db = self._client[database_name]
+        self.col = self.db.users
+
+    def new_user(self, id):
+        return dict(id=id, file_id=None, caption=None, thumbnail=None)
+
+    async def add_user(self, id):
+        user = self.new_user(id)
+        await self.col.insert_one(user)
+
+    async def is_user_exist(self, id):
+        user = await self.col.find_one({'id': int(id)})
+        return bool(user)
+
+    async def total_users_count(self):
+        count = await self.col.count_documents({})
+        return count
+
+    async def get_all_users(self):
+        all_users = self.col.find({})
+        return all_users
+
+    async def delete_user(self, user_id):
+        await self.col.delete_many({'id': int(user_id)})
+
+    async def set_thumbnail(self, id, file_id):
+        await self.col.update_one({'id': id}, {'$set': {'thumbnail': file_id}})
+
+    async def get_thumbnail(self, id):
+        user = await self.col.find_one({'id': int(id)})
+        return user.get('thumbnail') if user else None
+
+    async def set_caption(self, id, caption):
+        await self.col.update_one({'id': id}, {'$set': {'caption': caption}})
+
+    async def get_caption(self, id):
+        user = await self.col.find_one({'id': int(id)})
+        return user.get('caption') if user else None
+
+    async def delete_caption(self, id):
+        await self.col.update_one({'id': id}, {'$set': {'caption': None}})
+
+db = Database(DB_URL, DB_NAME)
