bff3fcb0464f989e58e30dc2b289f37a791fdd53
LEVYABDI <kevyabdi@gmail.com>
2025-08-13 12:49:59 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,13 +6,13 @@ import re, os
 
 id_pattern = re.compile(r'^.\d+$') 
 
-API_ID = os.environ.get("API_ID", "")
-API_HASH = os.environ.get("API_HASH", "")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-FORCE_SUB = os.environ.get("FORCE_SUB", "")
+API_ID = os.environ.get("API_ID", "26176218")
+API_HASH = os.environ.get("API_HASH", "4a50bc8acb0169930f5914eb88091736")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8271624089:AAHReeFOBWExbuPFVz7opSf52q6B8gHDcyQ")
+FORCE_SUB = os.environ.get("FORCE_SUB", "fpflims")
 
-DB_NAME = os.environ.get("DB_NAME", "")
-DB_URL = os.environ.get("DB_URL", "")
+DB_NAME = os.environ.get("DB_NAME", "kevyabdi20")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://kevyabdi20:<kevyabdi20>@kevyabdi.mymiztp.mongodb.net/?retryWrites=true&w=majority&appName=kevyabdi")
 
 FLOOD = int(os.environ.get("FLOOD", "10"))
 START_PIC = os.environ.get("START_PIC", "")
